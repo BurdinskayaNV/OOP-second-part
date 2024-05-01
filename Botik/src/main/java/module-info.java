1e481b3cd24ea3ad87a1b7c971539c3416a3520f
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2024-05-02 00:30:56 +0900

    Delete Botik directory

--- Botik/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.botik {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-
-
-    opens com.example.botik to javafx.fxml;
-    exports com.example.botik;
-}
